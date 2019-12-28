94a8fb09ebe4a71f57c330b225831bf7bbc375bf
Ziemowit S <ziemowit.slawinski@gmail.com>
2019-12-28 10:56:28 +0100

    regactored code

---
+++ brian/handlers/spike_event_handler.py
@@ -0,0 +1,16 @@
+from brian.handlers import BrianEventHandler
+from brian.utils import plot_spikes
+
+
+class SpikeEventHandler(BrianEventHandler):
+
+    def exec(self, *args, **kwargs):
+        spike_monitor = kwargs['spike_monitor']
+        print('spike!')
+        self.fig_spike = plot_spikes(spike_monitor, "output spikes", fig=self.fig)
+        self.output = spike_monitor.i
+
+    def pop(self):
+        result = self.output
+        self.output = []
+        return result
